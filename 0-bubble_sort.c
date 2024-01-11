d77e980cb4386db77affb53fc10b210f672ec79e
German <gerchureloco@gmail.com>
2024-01-11 15:34:28 -0300

    test 0 bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,25 +4,25 @@
 * the Bubble sort algorithm
 * @array: array of integers.
 * @size: size of the array.
-* Return: void 
+* Return: void
 */
 void bubble_sort(int *array, size_t size)
 {
 	size_t c;
 	int aux, trigger;
-	
+
 	while (trigger == 1)
 	{
-        trigger = 0;
+		trigger = 0;
    		for (c = 0; c < size; c++)
 		{
 			if (array[c] > array[c + 1])
 			{
 				aux = array[c];
 				array[c] = array[c + 1];
-				array[c + 1] = aux; 
-				printarray(array, size);
-                trigger = 1;
+				array[c + 1] = aux;
+				print_array(array, size);
+				trigger = 1;
 			}
 		}
 	}
