d7428e0c0a7a3fb009d035e33ed41de6c63d6ab4
German <gerchureloco@gmail.com>
2024-01-11 22:23:27 -0300

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,14 +13,12 @@ void insertion_sort_list(listint_t **list)
 	change2 = change1->next;
 	afchange2 = change2->next;
 	bfchange1 = change1->prev;
-	
 	if (list != NULL)
 	{
 		while (change1->next != NULL)
 		{
 			if (change1->n > change2->n)
-			{ 	
-
+			{
 				change2->prev = bfchange1;
 				change2->next = change1;
 				if (afchange2 != NULL)
@@ -30,15 +28,12 @@ void insertion_sort_list(listint_t **list)
 				if (bfchange1 != NULL)
 					bfchange1->next = change2;
 				else
-				{
 					*list = change2;
-				}
 				change1 = *list;
 				change2 = change1->next;
 				afchange2 = change2->next;
 				bfchange1 = change1->prev;
-				print_list(*list);
-			}
+				print_list(*list); }
 			else if (change1->next != NULL)
 			{
 				change1 = change1->next;
@@ -46,8 +41,7 @@ void insertion_sort_list(listint_t **list)
 				{
 					change2 = change1->next;
 					afchange2 = change2->next;
-					bfchange1 = change1->prev;
-				}
+					bfchange1 = change1->prev; }
 			}
 		}
 	}
