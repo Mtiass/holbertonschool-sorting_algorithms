b5d86d722df4fb28b17504ddd3e0499b17e7ee13
German <gerchureloco@gmail.com>
2024-01-11 22:13:21 -0300

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -34,6 +34,7 @@ void insertion_sort_list(listint_t **list)
 					*list = change2;
 				}	
 				
+			
 				change2 = change1->next;
 				afchange2 = change2->next;
 				bfchange1 = change1->prev;
@@ -46,6 +47,7 @@ void insertion_sort_list(listint_t **list)
 				afchange2 = change2->next;
 				bfchange1 = change1->prev;
 			}
+			
 		}
 	}
 }
