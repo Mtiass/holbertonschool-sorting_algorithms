940974bdb8f56281612597a2be232ed9fbabcbcc
German <gerchureloco@gmail.com>
2024-01-13 18:19:27 -0300

    3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,8 +12,7 @@
 */
 int lomuto(int *array, int start, int end, size_t size)
 {
-    size_t pivot = end;
-	int temp, count, count2 = 0;
+	int temp, count, count2 = 0, pivot = end;
 
 	for (count = start; count < end - 1; count++)
 	{
